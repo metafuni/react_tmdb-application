1fdab47023cdc44f0a35f4922a09df4b923ee757
Stefano Metafuni <metafunistefano@gmail.com>
2020-03-02 21:58:32 +0000

    basic working function

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,26 +7,40 @@ import Axios from 'axios';
 
 const Main = () => {
     const [movie, setMovie] = useState({});
-    const [id, setId] = useState(157336);
+    const [id, setId] = useState();
+    const [title, setTitle] = useState();
     const [loading, setLoading] = useState(true);
 
     const API_key = '3ee3dd446b8afd003b08f596ade66996';
     const url = `https://api.themoviedb.org/3/movie/${id}?&api_key=${API_key}`;
 
     const fetchMovie = async () => {
+        setId(157336);
         const result = await Axios(url);
         setMovie(result.data);
+    };
+
+    const updateMovie = async (e) => {
+        // setLoading(true);
+        e.preventDefault();
+        setTitle(e.target.elements.title.value);
+        console.log(title);
+        const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${API_key}&query=${e.target.elements.title.value}`;
+        const result = await Axios(searchUrl);
+        setMovie(result.data.results[0]);
+        console.log(result);
+
+        setTitle('');
     };
 
     useEffect(() => {
         fetchMovie();
         setLoading(false);
-    }, []);
+    }, [id]);
 
     console.log(movie);
     const backdropUrl = 'https://image.tmdb.org/t/p/original/' + movie.backdrop_path;
-    console.log(backdropUrl);
-    document.body.style.background = `url(${backdropUrl})`;
+    document.body.style.backgroundImage = `url(${backdropUrl})`;
 
     return (
         <div className="container">
@@ -35,7 +49,7 @@ const Main = () => {
                                 <img src={loadingLogo} width="150px" alt="The Movie Database"></img>
                             </div>
                         </span> : null}
-            <Search />
+            <Search updateMovie={updateMovie} />
             <Card movie={movie} />
         </div>
     )
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Logo from '../img/tmdb.svg';
 
-const Search = () => {
+const Search = ({ updateMovie }) => {
     return (
         <div className="search">
             <a href="https://www.themoviedb.org/?language=en-US" target="blank">
                 <img src={Logo} alt="TMDB movie database website" title="The Movie Database" width="180px"></img>
             </a>
-            <input 
-                type="text"
-                placeholder="Search Movie Title ..."
-            />
+            <form className="search-form" onSubmit={updateMovie}>
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="Search Movie Title ..."
+                    id="title"
+                />
+            </form>
         </div>
     )
 };
